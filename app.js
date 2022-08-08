414800c18c99617b135ac7639ab4741d966a51ab
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-08 10:03:26 +0600

    destructuring add

---
+++ app.js
@@ -0,0 +1,14 @@
+const brainStain = {
+    id: 1,
+    designation: 'Jr Web Developer',
+    name: 'Ruhul Juwel',
+    age: 24,
+    skills: {
+        learner: 'typscript, react native',
+        comfort: 'react node',
+        basic: 'html css js',
+    }
+}
+
+const { basic, learner } = brainStain.skills;
+console.log(basic);
